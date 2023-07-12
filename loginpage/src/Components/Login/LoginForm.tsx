8afe51a0c48ac958b4025f72125ab3b52f65cf95
Mohammad Bashirinia <mohammadbashirinia777@gmail.com>
2023-07-12 15:40:13 +0330

    Inputs added

--- loginpage/src/Components/Login/LoginForm.tsx
+++ loginpage/src/Components/Login/LoginForm.tsx
@@ -1,4 +1,4 @@
-import "./Login.css";
+﻿import "./Login.css";
 
 
 
@@ -15,6 +15,19 @@ const LoginForm = () => {
             <div className="header-line absolute top-[46%]">
 
             </div>
+
+            
+        </div>
+
+        <div className="w-full inputs px-10 pt-10 flex flex-col gap-5">
+            <input type="text" className="w-full input-text text-center" placeholder="نام کاربری" />
+            <input type="password" className="w-full input-text text-center" placeholder="کلمه عبور" />
+        </div>
+
+        <div className="flex justify-between px-10 py-5 pb-10">
+            <p>Check</p>
+
+            <button type="button">ورود</button>
         </div>
     </div>;
 }
